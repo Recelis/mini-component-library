1f79d1462d74225a42d7fcc875de41ba2ccc62fd
Jacky Lui <jackymlui@live.com>
2023-08-02 08:26:08 +1000

    WIP progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,28 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <div 
+          role="meter" 
+          aria-valuenow={value} 
+          aria-valuemin={0} 
+          aria-valuemax={100} 
+          aria-labelledby='Progress Bar'
+        >
+          <svg width="100"
+            height="100"
+            class="fill"
+            aria-hidden="true"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg">
+            <rect x="0"
+                  y="0"
+                  width={value}
+                  height="100%"
+                  fill="red"
+            >  
+            </rect>
+          </svg>
+        </div>;
 };
 
 export default ProgressBar;
