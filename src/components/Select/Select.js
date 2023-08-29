05f0befee0f18fc0cec297fc69f4f318cb4c58b6
Jacky Lui <jackymlui@live.com>
2023-08-30 06:00:11 +1000

    styling select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,29 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const SelectWrapper = styled.select`
+  appearance: none;
+  text-indent: 1px;
+  text-overflow: '';
+  background: #80808026;
+  border: solid 1px transparent;
+  border-radius: 8px;
+  padding: 12px 16px;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 19px;
+  letter-spacing: 0em;
+  text-align: left;
+`
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <SelectWrapper value={value} onChange={onChange}>
       {children}
-    </select>
+    </SelectWrapper>
   );
 };
 
