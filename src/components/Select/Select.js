7c5dd98a20a409cb356273fae8953b426941ddeb
Jacky Lui <jackymlui@live.com>
2023-09-04 07:49:59 +1000

    hidden native select with presentation value - issue with first option not able to be selected

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,29 +5,58 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
-const SelectWrapper = styled.select`
-  appearance: none;
-  text-indent: 1px;
-  text-overflow: '';
-  background: #80808026;
-  border: solid 1px transparent;
-  border-radius: 8px;
-  padding: 12px 16px;
-  font-family: Roboto;
-  font-size: 16px;
-  font-weight: 400;
-  line-height: 19px;
-  letter-spacing: 0em;
-  text-align: left;
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
 `
 
-const Select = ({ label, value, onChange, children }) => {
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  /* Allow the select to span the full height in Safari */
+  -webkit-appearance: none;
+`
+
+const PresentationalBit = styled.div`
+    color: ${COLORS.gray700};
+    background-color: ${COLORS.transparentGray15};
+    border: solid 1px transparent;
+    border-radius: 8px;
+    padding: 12px 16px;
+    font-size: 1rem;
+
+    display: flex;
+    align-items: center;
+
+    ${NativeSelect}:focus + & {
+      outline: 1px dotted #212121;
+      outline: 5px auto -webkit-focus-ring-color;
+    }
+
+    ${NativeSelect}:hover + & {
+      color: ${COLORS.black};
+    }
+`
+
+
+
+const Select = ({ id, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <SelectWrapper value={value} onChange={onChange}>
-      {children}
-    </SelectWrapper>
+    <Wrapper>
+      <NativeSelect id={id} value={displayedValue} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <Icon id={'chevron-down'} size={24} style={{paddingLeft: '52px'}}/>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
 
