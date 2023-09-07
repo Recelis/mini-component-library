89deb877007257f5f5b58bb7bbd5f15a417a87bb
Jacky Lui <jackymlui@live.com>
2023-09-08 06:26:37 +1000

    after watching video

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -10,16 +10,18 @@ const SIZES_STYLES = {
   'small': {
     fontSize: 'calc(14rem / 16)',
     iconSize: '16px',
-    paddingLeft: 'calc(8rem / 16)'
+    paddingLeft: 'calc(8rem / 16)',
+    borderThickness: 1,
   },
   'large': {
     fontSize: 'calc(18rem / 16)',
     iconSize: '24px',
-    paddingLeft: 'calc(12rem / 16)'
+    paddingLeft: 'calc(12rem / 16)',
+    borderThickness: 2,
   }
 }
 
-const InputWrapper = styled.div`
+const InputWrapper = styled.label`
   width: ${props => props.width}px;
   position: relative;
   height: ${props => props.iconSize};
@@ -33,23 +35,29 @@ const InputWrapper = styled.div`
 
 const StyledInput = styled.input`
   padding-left: calc(${props => props.paddingLeft} + ${props => props.iconSize});
-  width: 100%;
+  width: ${props => props.width};
   height: ${props => props.iconSize};
   border: 0;
   border-bottom: solid 1px grey;
-  z-index: 1;
   position: relative;
   background-color: transparent;
   outline-offset: 2px;
   color: inherit;
   font-weight: inherit;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
 `
 
 const IconWrapper = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  z-index: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: ${props => props.iconSize};
 `
 
 const IconInput = ({
@@ -58,21 +66,38 @@ const IconInput = ({
   width = 250,
   size,
   placeholder,
+  ...delegated
 }) => {
   return <InputWrapper
     width={width}
     iconSize={SIZES_STYLES[size].iconSize}
-  >
-      <StyledInput 
-        type={'text'} 
-        placeholder={placeholder} 
-        paddingLeft={SIZES_STYLES[size].paddingLeft} 
-        iconSize={SIZES_STYLES[size].iconSize}
-      />
-      <IconWrapper>
-        <Icon id={icon} size={SIZES_STYLES[size].iconSize} />
-      </IconWrapper>
-    </InputWrapper>
+  > 
+    <VisuallyHidden>{label}</VisuallyHidden>
+    <IconWrapper 
+      iconSize={SIZES_STYLES[size].iconSize}
+      width={width}
+    >
+      <Icon id={icon} size={SIZES_STYLES[size].iconSize} />
+    </IconWrapper>
+    <StyledInput
+      {...delegated}
+      type={'text'} 
+      placeholder={placeholder}
+      // Better to use style here with CSS Variables
+      /*
+      style={{
+          '--width': width + 'px',
+          '--height': styles.height / 16 + 'rem',
+          '--border-thickness': styles.borderThickness + 'px',
+          '--font-size': styles.fontSize / 16 + 'rem',
+        }}
+      */
+      paddingLeft={SIZES_STYLES[size].paddingLeft} 
+      iconSize={SIZES_STYLES[size].iconSize}
+      borderThickness={SIZES_STYLES[size].borderThickness}
+    />
+    
+  </InputWrapper>
 };
 
 export default IconInput;
