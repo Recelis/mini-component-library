6a8aa5dbab6a37263247d37a3fab63cb882a9e5b
Jacky Lui <jackymlui@live.com>
2023-09-07 21:40:53 +1000

    setting up icon input internal padding and sizes

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,23 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES_STYLES = {
+  'small': {
+    fontSize: 'calc(14rem / 16)',
+    iconSize: '16px',
+    paddingLeft: 'calc(8rem / 16)'
+  },
+  'large': {
+    fontSize: 'calc(18rem / 16)',
+    iconSize: '24px',
+    paddingLeft: 'calc(12rem / 16)'
+  }
+}
+
+const StyledInput = styled.input`
+  padding-left: calc(${props => props.paddingLeft} + ${props => props.iconSize});
+`
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +30,19 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return <div style={{
+    width,
+    fontSize: SIZES_STYLES[size].fontSize
+  }}>
+      <StyledInput 
+        type={'text'} 
+        placeholder={placeholder} 
+        paddingLeft={SIZES_STYLES[size].paddingLeft} 
+        iconSize={SIZES_STYLES[size].iconSize}
+      />
+      <Icon id={icon} size={SIZES_STYLES[size].iconSize} />
+        
+    </div>
 };
 
 export default IconInput;
