464a03c0b1a3c54364d86ee7e00baf6ae7cee916
Jacky Lui <jackymlui@live.com>
2023-09-08 06:00:02 +1000

    self-completed iconinput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -19,8 +19,37 @@ const SIZES_STYLES = {
   }
 }
 
+const InputWrapper = styled.div`
+  width: ${props => props.width}px;
+  position: relative;
+  height: ${props => props.iconSize};
+  color: ${COLORS.gray700};
+
+  &:hover {
+    font-weight: 700;
+    color: ${COLORS.black}
+  }
+`
+
 const StyledInput = styled.input`
   padding-left: calc(${props => props.paddingLeft} + ${props => props.iconSize});
+  width: 100%;
+  height: ${props => props.iconSize};
+  border: 0;
+  border-bottom: solid 1px grey;
+  z-index: 1;
+  position: relative;
+  background-color: transparent;
+  outline-offset: 2px;
+  color: inherit;
+  font-weight: inherit;
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
 `
 
 const IconInput = ({
@@ -30,19 +59,20 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return <div style={{
-    width,
-    fontSize: SIZES_STYLES[size].fontSize
-  }}>
+  return <InputWrapper
+    width={width}
+    iconSize={SIZES_STYLES[size].iconSize}
+  >
       <StyledInput 
         type={'text'} 
         placeholder={placeholder} 
         paddingLeft={SIZES_STYLES[size].paddingLeft} 
         iconSize={SIZES_STYLES[size].iconSize}
       />
-      <Icon id={icon} size={SIZES_STYLES[size].iconSize} />
-        
-    </div>
+      <IconWrapper>
+        <Icon id={icon} size={SIZES_STYLES[size].iconSize} />
+      </IconWrapper>
+    </InputWrapper>
 };
 
 export default IconInput;
